49e631bf9fd839af6ffcc717085afe1382127c17
Soni <Dsn2004@mail.ru>
2024-07-13 18:07:09 +0300

    Илье не понравился метод авторизации

--- backend/database/avto.sql
+++ backend/database/avto.sql
@@ -1,22 +1,20 @@
 --1 variant
 DROP FUNCTION authorization(_login VARCHAR(60),_passwd VARCHAR(60))
 
-CREATE OR REPLACE FUNCTION authorization(_login VARCHAR(60),_passwd VARCHAR(60))
+CREATE OR REPLACE FUNCTION authorization(_login VARCHAR(60),_passwd VARCHAR(60),
+										 OUT id_user INT, OUT name VARCHAR(50), OUT surname VARCHAR(50))
 									
-RETURNS RECORD AS $$
-DECLARE 
-	res RECORD;
+RETURNS SETOF RECORD AS $$
 BEGIN
-	SELECT us.id_user, us.name, us.surname INTO res FROM Users AS us WHERE us.login = _login AND
+	RETURN QUERY
+	SELECT us.id_user, us.name, us.surname FROM Users AS us WHERE us.login = _login AND
 	us.hash_passwd = crypt(_passwd,us.hash_passwd);
 	IF NOT FOUND THEN  
 	RAISE EXCEPTION 'Пользователь с указанными логином и паролем не найден';
 	END IF;
-	RETURN res;
 EXCEPTION 
 	WHEN others THEN 
 	RAISE NOTICE 'Возникло исключение: %', SQLERRM;	
-	RETURN NULL;
 END;
 $$ LANGUAGE plpgsql; 
 
