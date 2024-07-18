639382002ba2b51102a1989ceb04260483c346e4
Evgeniy Anisiforov <y.anisiforov@mail.ru>
2024-07-18 19:14:54 +0300

    добавил кеширование в локальное хранилище

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './redux/index.ts'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import {PersistGate} from "redux-persist/integration/react"
+import store, { persistor } from "./redux/index.ts"
+import App from "./App.tsx"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <BrowserRouter basename='/Grapefruit_task_manager/'>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/Grapefruit_task_manager/">
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>
 )
--- frontend/src/redux/index.ts
+++ frontend/src/redux/index.ts
@@ -1,21 +1,59 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { AuthApi } from "./API/AuthApi";
-import { RegApi } from "./API/RegApi";
-import { KanbanApi } from "./API/KanbanApi";
-import  statusAuthSlice from "./statusAuth-slice";
-import { CaptchaApi } from "./API/CaptchaApi";
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import { AuthApi } from "./API/AuthApi"
+import { RegApi } from "./API/RegApi"
+import { KanbanApi } from "./API/KanbanApi"
+import statusAuthSlice from "./statusAuth-slice"
+import { CaptchaApi } from "./API/CaptchaApi"
 
-export const store = configureStore({
-    reducer:{
-        [AuthApi.reducerPath]: AuthApi.reducer,
-        [RegApi.reducerPath]: RegApi.reducer,
-        [KanbanApi.reducerPath]: KanbanApi.reducer,
-        [CaptchaApi.reducerPath]: CaptchaApi.reducer,
-        statusAuth: statusAuthSlice,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(AuthApi.middleware, RegApi.middleware, KanbanApi.middleware, CaptchaApi.middleware),
-    devTools: true
+const rootReducer = combineReducers({
+  [AuthApi.reducerPath]: AuthApi.reducer,
+  [RegApi.reducerPath]: RegApi.reducer,
+  [KanbanApi.reducerPath]: KanbanApi.reducer,
+  [CaptchaApi.reducerPath]: CaptchaApi.reducer,
+  statusAuth: statusAuthSlice,
 })
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: [
+    AuthApi.reducerPath,
+    RegApi.reducerPath,
+    KanbanApi.reducerPath,
+    CaptchaApi.reducerPath,
+  ],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(
+      AuthApi.middleware,
+      RegApi.middleware,
+      KanbanApi.middleware,
+      CaptchaApi.middleware
+    ),
+  devTools: true,
+})
+export const persistor = persistStore(store)
+export default store
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
